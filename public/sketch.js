0fde3839fdc7444ddc083caac90a4e4706ede847
Trinidad <nestrimo@gamil.com>
2020-10-22 21:46:07 -0500

    preuba de tamanio heroku

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,12 @@
 let capture;
 
 function setup() {
-  createCanvas(480, 480);
-  capture = createCapture(VIDEO);
-  capture.hide();
+    createCanvas(480, 480);
+    capture = createCapture(VIDEO);
+    capture.hide();
 }
 
 function draw() {
-  image(capture, 0, 0, width, width * capture.height / capture.width);
-  filter(INVERT);
+    image(capture, 0, 0);
+    filter(INVERT);
 }
