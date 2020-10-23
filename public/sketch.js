0525abdeed2f6f94fb8f85692cad3934840eb02e
Trinidad <nestrimo@gamil.com>
2020-10-23 12:14:50 -0500

    cambio del modelo

--- public/sketch.js
+++ public/sketch.js
@@ -3,33 +3,31 @@ let classifier;
 let modelURL = './model/';
 let label = "Cargando...";
 
-// STEP 1: Load the model!
+// carga el modelo
 function preload() {
     classifier = ml5.imageClassifier(modelURL + 'model.json');
 }
 
 function setup() {
     createCanvas(640, 520);
-    // Create the video
+
     video = createCapture(VIDEO);
     video.hide();
 
-    // STEP 2.1: Start classifying
+
     classifyVideo();
 }
 
-// STEP 2.2 classify!
+
 function classifyVideo() {
     classifier.classify(video, gotResults);
 }
 
 function draw() {
     background(0);
-
-    // Draw the video
     image(video, 0, 0);
 
-    // STEP 4: Draw the label
+
     textSize(32);
     textAlign(CENTER, CENTER);
     fill(255);
@@ -37,14 +35,14 @@ function draw() {
 }
 
 
-// STEP 3: Get the classification!
+
 function gotResults(error, results) {
-    // Something went wrong!
+
     if (error) {
         console.error(error);
         return;
     }
-    // Store the label and classify again!
+
     label = results[0].label;
     console.log(results[0].label);
     classifyVideo();
